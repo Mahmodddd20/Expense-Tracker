bc317cd91e452900ae1cafbacc367e6f4c6a3ffb
mahmoud <mahmodddd20@gmail.com>
2021-04-28 12:10:48 +0300

    css animation

--- main.js
+++ main.js
@@ -61,11 +61,12 @@ function addTransactionDOM(transaction) {
     item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
     item.innerHTML = `
-    ${transaction.text} ${sign}${Math.abs(
-        transaction.amount
-    )} <button class="delete-btn" onclick="removeTransaction(${
+    <button class="delete-btn" onclick="removeTransaction(${
         transaction.id
     })">X</button>
+    ${transaction.text} ${sign}${Math.abs(
+        transaction.amount
+    )} 
   `;
 
     list.appendChild(item);
